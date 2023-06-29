aa338a0c63c56de6e9576b2ada3748c5edafe825
Ryan Adi Putra <ryannadiputraa@gmail.com>
2023-06-29 11:10:20 +0800

    feat: user service

--- app/user/service/user_service.go
+++ app/user/service/user_service.go
@@ -2,8 +2,11 @@ package service
 
 import (
 	"context"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/ryanadiputraa/zenboard/domain"
+	log "github.com/sirupsen/logrus"
 )
 
 type userService struct {
@@ -17,13 +20,29 @@ func NewUserService(repository domain.UserRepository) domain.UserService {
 }
 
 func (s *userService) CreateOrUpdateUserIfExists(ctx context.Context, user domain.User) (createdUser domain.User, err error) {
+	user.ID = uuid.NewString()
+	user.BoardLimit = domain.DEFAULT_BOARD_LIMIT
+	user.CreatedAt = time.Now()
+
+	createdUser, err = s.repository.Save(ctx, user)
+	if err != nil {
+		log.Error("fail to create or update user: ", err)
+	}
 	return
 }
 
 func (s *userService) ListUserWithinIds(ctx context.Context, ids []string) (users []domain.User, err error) {
+	users, err = s.repository.List(ctx, ids)
+	if err != nil {
+		log.Error("fail to get list of users: ", err)
+	}
 	return
 }
 
 func (s *userService) FindUserByID(ctx context.Context, userID string) (user domain.User, err error) {
+	user, err = s.repository.FindByID(ctx, userID)
+	if err != nil {
+		log.Error("fail to find user: ", err)
+	}
 	return
 }
